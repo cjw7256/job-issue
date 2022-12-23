e17290aa02f7e95b756a63f30c7b6a41575ffe2b
leeeda <ldj6069@naver.com>
2022-12-23 11:03:56 +0900

    commit  후 수정 &  지원자 관리창 navbar 연결

---
+++ src/main/java/sample/project/jobissue/controller/ApplicantManage.java
@@ -0,0 +1,20 @@
+package sample.project.jobissue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/applicantManage")
+public class ApplicantManage {
+
+	@GetMapping
+	public String manageOpening(Model model, HttpServletRequest req){
+		return "applicantManage";
+	}
+}
