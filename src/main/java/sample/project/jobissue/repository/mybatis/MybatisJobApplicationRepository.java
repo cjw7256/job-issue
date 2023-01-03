77f6681b5d19fbf46f5b6279b9284ddc7b1239ce
Yoon <witteny24@gmail.com>
2023-01-03 15:10:11 +0900

    오류수정

---
+++ src/main/java/sample/project/jobissue/repository/mybatis/MybatisJobApplicationRepository.java
@@ -0,0 +1,41 @@
+package sample.project.jobissue.repository.mybatis;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import sample.project.jobissue.domain.JobItem;
+import sample.project.jobissue.domain.ResumeItem;
+import sample.project.jobissue.repository.JobApplicationRepository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MybatisJobApplicationRepository implements JobApplicationRepository {
+
+	private final JobApplicationMapper jobApplicationMapper;
+
+
+	@Override
+	public ResumeItem selectByUserResume(int userCode) {
+		// TODO Auto-generated method stub
+		ResumeItem resumeItem = jobApplicationMapper.selectByUserResume(userCode);
+		log.info("resumeItem {}",resumeItem);
+		return resumeItem;
+	}
+
+
+
+
+	@Override
+	public JobItem insertSubmitResume(int corCode, int announcementCode, int userCode) {
+		// TODO Auto-generated method stub
+		log.info("10");
+		jobApplicationMapper.insertSubmitResume(corCode, announcementCode, userCode);
+		return null;
+	}
+
+	
+	
+
+}
