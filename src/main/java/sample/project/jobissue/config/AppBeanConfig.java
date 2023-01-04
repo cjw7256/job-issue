d20402cc3ca4ac385f0d3f91e92ab4671b999ba9
Yoon <witteny24@gmail.com>
2023-01-04 20:55:54 +0900

    불필요한 코드 정리 및 admin레포지터리 빈 등록 추가

--- src/main/java/sample/project/jobissue/config/AppBeanConfig.java
+++ src/main/java/sample/project/jobissue/config/AppBeanConfig.java
@@ -3,11 +3,14 @@ package sample.project.jobissue.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import sample.project.jobissue.repository.AdminRepository;
 import sample.project.jobissue.repository.JobApplicationRepository;
 import sample.project.jobissue.repository.JobRepository;
 import sample.project.jobissue.repository.ResumeRepository;
+import sample.project.jobissue.repository.mybatis.AdminMapper;
 import sample.project.jobissue.repository.mybatis.JobApplicationMapper;
 import sample.project.jobissue.repository.mybatis.JobMapper;
+import sample.project.jobissue.repository.mybatis.MybatisAdminRepository;
 import sample.project.jobissue.repository.mybatis.MybatisJobApplicationRepository;
 import sample.project.jobissue.repository.mybatis.MybatisJobRepository;
 import sample.project.jobissue.repository.mybatis.MybatisResumeRepository;
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class AppBeanConfig {
 	private final JobMapper jobMapper;
 	private final ResumeMapper resumeMapper;
+	private final AdminMapper adminMapper;
 	private final JobApplicationMapper jobApplicationMapper;
 	
 	@Bean
@@ -29,6 +33,11 @@ public class AppBeanConfig {
 	@Bean
 	public ResumeRepository resumeRepository() {
 		return new MybatisResumeRepository(resumeMapper);
+	}
+	
+	@Bean
+	public AdminRepository adminRepository() {
+		return new MybatisAdminRepository(adminMapper);
 	}
 	
 	@Bean
